6be2e1b7a3c30c462cc02406adaff94d2f352cfa
Vali Borlea <valentin.borlea@sap.com>
2018-05-06 13:02:33 +0300

    initial commit server app

---
+++ mapp-server/src/main/java/com/mapp/MappSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.mapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappSpringBootApplication.class, args);
+	}
+}
