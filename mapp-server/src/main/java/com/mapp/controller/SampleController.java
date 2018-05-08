47e5e1066c5c05965c17b3c1b1911add53e518e6
Vali Borlea <valentin.borlea@sap.com>
2018-05-08 23:05:08 +0300

    controllers for user and data

--- mapp-server/src/main/java/com/mapp/controller/SampleController.java
+++ mapp-server/src/main/java/com/mapp/controller/SampleController.java
@@ -1,20 +1,49 @@
 package com.mapp.controller;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SampleController
 {
-	@RequestMapping("/sample2")
-	@ResponseBody
-	public String sample()
+
+	@RequestMapping("/test")
+	public User sample()
 	{
-		return "Hello World!";
+		return new User("vvv", 15);
+	}
+
+	class User{
+		private String name;
+		private int age;
+
+		public User(String name, int age)
+		{
+			this.name = name;
+			this.age = age;
+		}
+
+		public String getName()
+		{
+			return name;
+		}
+
+		public void setName(String name)
+		{
+			this.name = name;
+		}
+
+		public int getAge()
+		{
+			return age;
+		}
+
+		public void setAge(int age)
+		{
+			this.age = age;
+		}
 	}
 }
